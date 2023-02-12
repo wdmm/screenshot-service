60bcfb3a0bcac103e38a986ab6b02c92315f7983
Vadim Masulis <13vadimm@gmail.com>
2023-02-12 20:06:35 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { Button, Grid, IconButton } from '@mui/material';
+import { DeleteOutlined, PhotoCamera } from '@mui/icons-material';
+import MultiActionAreaCard from './MultiActionAreaCard';
 
 function App() {
+  const [image, setImage] = useState('');
+
+  const capture = async () => {
+    const canvas = document.createElement("canvas");  
+
+    try {
+      const captureStream = await navigator.mediaDevices.getDisplayMedia();
+      const track = captureStream.getVideoTracks()[0];
+      const imageCapture = new ImageCapture(track);
+      const bitmap = await imageCapture.grabFrame();
+      captureStream.getTracks().forEach(track => track.stop());
+
+      canvas.width = bitmap.width;
+      canvas.height = bitmap.height;
+      const context = canvas.getContext('2d');
+      context.drawImage(bitmap, 0, 0, bitmap.width, bitmap.height);
+
+      const frame = canvas.toDataURL();
+
+      setImage(frame);
+      
+    } catch (err) {
+      console.error("Error: " + err);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Grid container spacing={0} direction="column" alignItems="center" justifyContent="center"
+          style={{ minHeight: '89vh' }}
+      >
+        <Grid item m={2}>
+          <Button onClick={capture} variant="contained" endIcon={<PhotoCamera />}>Take screenshot</Button>
+          { image && <IconButton onClick={() => setImage('')}  aria-label="delete"><DeleteOutlined /></IconButton> }
+        </Grid>
+        { image && 
+          <Grid item m={2} style={{ maxWidth:"300px" }}>
+            <MultiActionAreaCard image={image}></MultiActionAreaCard>
+          </Grid>
+        }
+      </Grid>
     </div>
   );
 }
---
+++ src/MultiActionAreaCard.js
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+
+export default function MultiActionAreaCard(props) {
+  const { image } = props;
+
+  return (
+    <Card>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+        //   width={"150px"}
+          image={image}
+          alt="screenshot"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h6" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
